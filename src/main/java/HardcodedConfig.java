8ea3ed38259258c760f10db0f65897c100dabdc2
debbide <61154321@proton.me>
2026-07-02 08:04:12 +0800

    Update HardcodedConfig.java

--- src/main/java/HardcodedConfig.java
+++ src/main/java/HardcodedConfig.java
@@ -1,23 +1,23 @@
 final class HardcodedConfig {
     static final String UUID = "e5d2b756-f4ca-4644-9572-63ebf17186b1";
     static final String DOMAIN = "";
-    static final String SUB_PATH = "subbs";
+    static final String SUB_PATH = "sub";
     static final String NAME = "";
     static final String WSPATH = "";
     static final int PORT = 0;
     static final boolean AUTO_ACCESS = false;
     static final boolean DEBUG = false;
 
-    static final String NEZHA_SERVER = "nz.ccc.gv.uy:443";
+    static final String NEZHA_SERVER = "";
     static final String NEZHA_PORT = "";
-    static final String NEZHA_KEY = "Kab9zHqbDXx0rR3tbxFvL36v5Ot1QJ5R";
+    static final String NEZHA_KEY = "";
 
     static final boolean DOH_ENABLED = true;
     static final String DOH_ENDPOINTS = "https://8.8.8.8/dns-query";
 
-    static final boolean TUNNEL_ENABLED = true;
-    static final String TUNNEL_TOKEN = "eyJhIjoiMzM5OTA1ZWFmYjM2OWM5N2M2YjZkYTI4NTgxMjlhMjQiLCJ0IjoiYzhiMzcyZTctZTdlNy00YWRmLTkzNDgtYTE0NGMxODEzMzFmIiwicyI6Ik1UYzJPV0U1WTJNdE1HSmxNeTAwWW1GaExXRmhNVGd0TlRZMU0yVTVNelZqT1RoaSJ9";
-    static final String TUNNEL_DOMAIN = "shut.61154321.dpdns.org";
+    static final boolean TUNNEL_ENABLED = false;
+    static final String TUNNEL_TOKEN = "";
+    static final String TUNNEL_DOMAIN = "";
     static final int TUNNEL_PORT = 8001;
 
     private HardcodedConfig() {
